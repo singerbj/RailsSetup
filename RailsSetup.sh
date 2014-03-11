f3834a2ee60dcc83a4af82d70c01a0b23001b955
singerbj <singerbj@uwec.edu>
2014-03-10 22:35:08 -0500

    Update RailsSetup.sh

--- RailsSetup.sh
+++ RailsSetup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get install curl
 \curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+source /etc/profile.d/rvm.sh
 rvm requirements
 rvm install ruby
 rvm use ruby --default

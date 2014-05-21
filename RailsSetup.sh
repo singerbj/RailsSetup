7da5fe56723b83af1b62b2fe28588d4c7b9e065f
Benjamin Singer <singerbj@benjamin-elementaryos.(none)>
2014-05-20 23:05:56 -0500

    fixed sudo issues

--- RailsSetup.sh
+++ RailsSetup.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
 sudo apt-get install curl
-sudo \curl -L https://get.rvm.io | bash -s stable
-sudo source /etc/profile.d/rvm.sh
-sudo rvm requirements
-sudo rvm install ruby
-sudo rvm use ruby --default
-sudo rvm rubygems current
-sudo gem install rails --no-rdoc --no-ri
-sudo echo "done"
+curl -L https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install ruby
+rvm use ruby --default
+rvm rubygems current
+gem install rails --no-rdoc --no-ri
+echo "done"
